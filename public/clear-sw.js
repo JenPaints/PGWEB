a8e7dab81dcd5f0f605ed50614afdecb01591f34
Jenpaints <priyankajenifer24@gmail.com>
2025-08-03 01:43:19 +0530

    work

---
+++ public/clear-sw.js
@@ -0,0 +1,8 @@
+// Clear any existing service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...fontFamily.sans],
+      },
+      borderRadius: {
+        DEFAULT: "8px",
+        secondary: "4px",
+        container: "12px",
+      },
+      boxShadow: {
+        DEFAULT: "0 1px 4px rgba(0, 0, 0, 0.1)",
+        hover: "0 2px 8px rgba(0, 0, 0, 0.12)",
+      },
+
+      spacing: {
+        "form-field": "16px",
+        section: "32px",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      boxShadow: ["hover", "active"],
+    },
+  },
+};
